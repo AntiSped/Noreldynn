ad0c1033b2e5a22279c036f10fe5049803262871
404 <63125641+shireishi@users.noreply.github.com>
2020-11-09 15:41:17 -0700

    Adding Level Determination

--- Python Scripts/read_excel.py
+++ Python Scripts/read_excel.py
@@ -1,14 +1,28 @@
 import pandas as pd
+import math
 
 sheet = pd.read_excel('Sources/Noreldynn_-_Stat_Calculators.xlsx', sheet_name=None)
 
-for i in sheet.keys():
-    print(i)
+# for i in sheet.keys():
+    # print(i)
 
-print("\n")
-print(sheet["Player_Races"])
-print(sheet["Player_Races"]["Oni"])
+currentEP = input("Current Player EP : ")
+currentEP = int(currentEP)
 
-with open("test.txt", 'a') as file:
-    for i in sheet.keys():
-        file.write(str(sheet[i]))
+eptable = sheet["EP_Table"]
+
+for row in eptable["Needed EP"].keys():
+    row = int(row)
+    if row != 125:
+        if currentEP < math.floor(eptable["Needed EP"][row]):
+            # print("Completed.")
+            # print("{} is less than {}".format(currentEP, math.floor(eptable["Needed EP"][row])))
+            print("The player is {} experience points away from Level {}".format(int(eptable["Needed EP"][row])-currentEP, eptable["Level"][row]))
+            break
+    else:
+        if currentEP < eptable["Needed EP"][row]:
+            print("Completed")
+
+
+# for key in sheet["Player_Races"]["Oni"].keys():
+    # print("{} >> {}".format(sheet["Player_Races"]["Stats"][key], sheet["Player_Races"]["Oni"][key]))
