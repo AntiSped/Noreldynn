7f5f7a7106762129d1e09cd21c9b08be81424847
404 <63125641+shireishi@users.noreply.github.com>
2020-11-09 17:37:58 -0700

    Completed the Formulas

--- Sources/formulas.cs
+++ Sources/formulas.cs
@@ -3,19 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-class OniFormulas {
+public class Formulas 
+{
 
-    public static int Vitality(int level_modifier, int brawn, int toughness, int power_level) {
+    public static double Vitality(double level_modifier, int brawn, int toughness, int power_level) 
+    {
 
-        return((50*level_modifier)+(5*(brawn+toughness)*power_level));
+        return ((50 * level_modifier) + (5 * (brawn + toughness) * power_level));
 
     }
-    static void Main(string[] args){
 
-        int test = Vitality(level_modifier: 3578, brawn: 1114, toughness: 167, power_level: 125);
-        Console.WriteLine(test);
+    public static double Aether(double knowledge)
+    {
+
+        return (1000 * (1 + (0.0005 * knowledge)));
+
+    }
+
+    public static double magicArmor(double vitality, double mana_armor_amount) 
+    {
+
+        return (vitality * mana_armor_amount);
+
+    }
+
+    public static double physicalStat(double level_modifier, double dexterity, double brawn, double power_level)
+    {
+
+        return ((10 * level_modifier) + (1 * (dexterity + brawn) * power_level));
+
+    }
+
+    public static double arcanaStat(double level_modifier, double dexterity, double knowledge, double power_level)
+    {
+
+        return ((10 * level_modifier) + (1 * (dexterity + knowledge) * power_level));
+
+    }
+
+    public static double Defense(double level_modifier, double brawn, double toughness, double power_level)
+    {
+
+        return ((10 * level_modifier) + (1 * (brawn + toughness) * power_level));
+
+    }
+
+    public static double magicResist(double level_modifier, double knowledge, double toughness, double power_level)
+    {
+
+        return ((10 * level_modifier) + (1 * (knowledge + toughness) * power_level));
+
+    }
+
+    static void Main(string[] args)
+    {
+
+        double vitality = Vitality(level_modifier: 3578.5262, brawn: 1114, toughness: 167, power_level: 125);
+        Console.WriteLine(vitality);
+
+        double aether = Aether(knowledge: 1504);
+        Console.WriteLine(aether);
     }
 }
+
+public class OniAttributes {}
 
 public class WerewolfFormulas{}
 
