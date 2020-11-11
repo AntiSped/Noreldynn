b05ca911a061b9bb6573a152545582514e9042fd
404 <63125641+shireishi@users.noreply.github.com>
2020-11-10 20:29:02 -0700

    Moving information

--- Python Scripts/main.py
+++ Python Scripts/main.py
@@ -3,8 +3,13 @@ import ast
 import pandas as pd
 
 sheet = pd.read_excel('Sources/Noreldynn_-_Stat_Calculators.xlsx', sheet_name=None)
+keys = sheet["Player_Races"].keys()
 
-for key in sheet["Player_Races"].keys():
+
+for key in keys:
     with open("Sources/{}_data.json".format(key), 'r') as file:
         print(sheet["Player_Races"][key])
-        print(json.dumps(ast.literal_eval(file.read()), sort_keys=True, indent=4, default=str))
+        data = json.dumps(ast.literal_eval(file.read()), sort_keys=True, indent=4, default=str)
+        print(data)
+        with open("Sources/{}_data.json".format(key), 'w') as file:
+            file.write(str(data))
