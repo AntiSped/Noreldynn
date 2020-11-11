07f2f034e6997461ff6f30ef4f034a32c9331deb
404 <63125641+shireishi@users.noreply.github.com>
2020-11-10 17:37:07 -0700

    Race Information
    created a python script to read the entire excell sheet and convert all values into json format for their own respective files.

--- Python Scripts/read_excel.py
+++ Python Scripts/read_excel.py
@@ -3,26 +3,70 @@ import math
 
 sheet = pd.read_excel('Sources/Noreldynn_-_Stat_Calculators.xlsx', sheet_name=None)
 
-# for i in sheet.keys():
-    # print(i)
+def keys():
+    for i in sheet.keys():
+        print(i)
 
-currentEP = input("Current Player EP : ")
-currentEP = int(currentEP)
+def deep_keys():
+    for i in sheet["Player_Races"].keys():
+        print(i)
 
-eptable = sheet["EP_Table"]
+def exp_table():
+    currentEP = input("Current Player EP : ")
+    currentEP = int(currentEP)
 
-for row in eptable["Needed EP"].keys():
-    row = int(row)
-    if row != 125:
-        if currentEP < math.floor(eptable["Needed EP"][row]):
-            # print("Completed.")
-            # print("{} is less than {}".format(currentEP, math.floor(eptable["Needed EP"][row])))
-            print("The player is {} experience points away from Level {}".format(int(eptable["Needed EP"][row])-currentEP, eptable["Level"][row]))
-            break
+    eptable = sheet["EP_Table"]
+
+    for row in eptable["Needed EP"].keys():
+        row = int(row)
+        if row != 125:
+            if currentEP < math.floor(eptable["Needed EP"][row]):
+                print("The player is {} experience points away from Level {}".format(int(eptable["Needed EP"][row])-currentEP, eptable["Level"][row]))
+                break
+        else:
+            if currentEP < eptable["Needed EP"][row]:
+                print("Completed")
+
+def oni_information():
+    for key in sheet["Player_Races"]["Oni"].keys():
+        print("{} >> {}".format(sheet["Player_Races"]["Stats"][key], sheet["Player_Races"]["Oni"][key]))
+
+def test():
+    keys = sheet["Player_Races"]["Stats"].keys()
+
+    print(keys.stop)
+
+def type_script():
+    keys = sheet["Player_Races"].keys()
+
+    print(keys)
+ 
+    for race in sheet["Player_Races"].keys():
+        print(race)
+        print(sheet["Player_Races"][race])
+
+        dict = {}
+        for key in sheet["Player_Races"][race].keys():
+            dict[sheet["Player_Races"]["Stats"][key]] = sheet["Player_Races"][race][key]
+        
+        with open("{}_data.json".format(race), 'a') as file:
+            file.write(str(dict))
+
+def start():
+    rsp = input("Keys / Experience Table / Oni Information / Type Script / Deep Keys >> ")
+    if rsp == "keys" or rsp == "Keys":
+        keys()
+    elif rsp == "experience table" or rsp == "Experience table" or rsp == "Experience Table":
+        exp_table()
+    elif rsp == "oni information" or rsp == "Oni information" or rsp == "Oni Information":
+        oni_information()
+    elif rsp == "type script":
+        type_script()
+    elif rsp == "deep keys":
+        deep_keys()
+    elif rsp == "test":
+        test()
     else:
-        if currentEP < eptable["Needed EP"][row]:
-            print("Completed")
+        raise Exception("Failed to determine what task to complete.")
 
-
-# for key in sheet["Player_Races"]["Oni"].keys():
-    # print("{} >> {}".format(sheet["Player_Races"]["Stats"][key], sheet["Player_Races"]["Oni"][key]))
+start()
